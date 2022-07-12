86d159d7d4d7e68caea640462b97cb6c964e9b1f
Dima <soloveid87@mail.ru>
2022-07-12 10:10:12 +1000

    load balancer

--- photo-app-api-users/src/main/java/com/dima/photoappapi/users/ui/controllers/UsersController.java
+++ photo-app-api-users/src/main/java/com/dima/photoappapi/users/ui/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.dima.photoappapi.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
     public String status() {
-        return "working";
+        return "working on port " + environment.getProperty("local.server.port");
     }
 }
