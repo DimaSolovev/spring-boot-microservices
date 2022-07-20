da836e7ef59e6742d234c430847e3767db3a06b3
Dima <soloveid87@mail.ru>
2022-07-20 16:09:02 +1000

    feign errors handling

--- photo-app-api-users/src/main/java/com/dima/photoappapi/users/data/AlbumsServiceClient.java
+++ photo-app-api-users/src/main/java/com/dima/photoappapi/users/data/AlbumsServiceClient.java
@@ -1,6 +1,10 @@
 package com.dima.photoappapi.users.data;
 
 import com.dima.photoappapi.users.ui.model.AlbumResponseModel;
+import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +13,44 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = "albums-ws", fallback = AlbumsFallback.class)
+@FeignClient(name = "albums-ws", fallback = AlbumsFallbacFacktory.class)
 public interface AlbumsServiceClient {
 
-    @GetMapping("/users/{id}/albums")
+    @GetMapping("/users/{id}/albumss")
     public List<AlbumResponseModel> getAlbums(@PathVariable String id);
 }
 
 @Component
-class AlbumsFallback implements AlbumsServiceClient{
+class AlbumsFallbacFacktory implements FallbackFactory<AlbumsServiceClient> {
+
+    @Override
+    public AlbumsServiceClient create(Throwable cause) {
+        return new AlbumsServiceClientFallback(cause);
+    }
+}
+
+class AlbumsServiceClientFallback implements AlbumsServiceClient {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final Throwable cause;
+
+    public AlbumsServiceClientFallback(Throwable cause) {
+        this.cause = cause;
+    }
+
     @Override
     public List<AlbumResponseModel> getAlbums(String id) {
+        // TODO Auto-generated method stub
+
+        if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+            logger.error("404 error took place when getAlbums was called with userId: " + id + ". Error message: "
+                    + cause.getLocalizedMessage());
+        } else {
+            logger.error("Other error took place: " + cause.getLocalizedMessage());
+        }
+
         return new ArrayList<>();
     }
+
 }
